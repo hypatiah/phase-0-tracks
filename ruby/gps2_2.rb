f63d424b726c08b8386c60e774e5387c0929769c
hypatiah <danielle.hassid@gmail.com>
2016-05-17 17:48:14 -0700

    Create grocery list and methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -8,18 +8,14 @@
   #list.default = 1
   # print the list to the console by using the method at the bottom
 # output: our list as a hash with key value pairs of the item and its resp quantity
-puts "What do you want to get at the store?"
-response = gets.chomp
-
 def create_list(list)
 	list_split = list.split(' ')
 	arry = []
 	hsh = {}
 	list_split.each {|item| hsh[item] = 1}
 	hsh.default = 1
-	puts hsh
+	hsh
 end
-create_list(response)
 
 
 # Method to add an item to a list
@@ -28,8 +24,14 @@ create_list(response)
 	#prompt user which item want to add and optional quantity
 	#list[new_item] = optional_quantity
 # output: updated hash with the new item/quantity
-
-def add
+def add_item(list)
+	puts "Which item would you like to add?"
+	new_item = gets.chomp
+	puts "How many #{new_item}s do you want"
+	quantity = gets.chomp
+	list[new_item] = quantity
+	list
+end
 
 # Method to remove an item from the list
 # input: item name we want to remove
@@ -38,6 +40,12 @@ def add
 	#remove item
 	#list.delete("item name")
 # output: updated list with the removed item
+def remove_item(list)
+	puts "Which item would you like to remove?"
+	delete_me = gets.chomp
+	list.delete(delete_me)
+	list
+end
 
 # Method to update the quantity of an item
 # input: item name, new item quantity
@@ -46,9 +54,38 @@ def add
 	#prompt user for the new quantity they want of the item
 	#list["itemname"] = updated_quantity
 # output: updated list with updated quantity
+def update_quantity(list)
+	puts "Which item would you like to update the quantity of?"
+	item_name = gets.chomp
+	puts "Whats your new quantity?"
+	new_quantity = gets.chomp
+	list[item_name] = new_quantity
+	list
+end
 
 # Method to print a list and make it look pretty
 # input: list
 # steps: 
 	#list.each {|item, quantity| puts "You need to buy #{quantity} #{item}s!"}
 # output: pretty, readable list
+def pretty_list(list)
+	puts "You need to buy:"
+	list.each {|item, quantity| puts "#{item}: #{quantity}"}
+end
+
+#DRIVER CODE
+puts "What do you want to get at the store?"
+response = gets.chomp
+list = create_list(response)
+list2 = add_item(list)
+list3 = remove_item(list2)
+list4 = update_quantity(list3)
+final_list = pretty_list(list4)
+
+
+#What did you learn about pseudocode from working on this challenge?
+#What are the tradeoffs of using arrays and hashes for this challenge?
+#What does a method return?
+#What kind of things can you pass into methods as arguments?
+#How can you pass information between methods?
+#What concepts were solidified in this challenge, and what concepts are still confusing?
