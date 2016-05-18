b332335fbd952fdd57979b2c43b8da0343e816d9
hypatiah <danielle.hassid@gmail.com>
2016-05-18 16:35:28 -0700

    Create instance methods for Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,34 @@ class Puppy
     toy
   end
 
+  def speak(intgr)
+  	intgr.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  #input: human years
+  #output: dog years
+  def dog_years(hum_age)
+  	dog_age = hum_age * 7
+  	puts "Your dog age is #{dog_age}"
+  end
+
+  def shake(name)
+  	puts "Nice to meet you #{name}!"
+  end
+
 end
 
+
+#DRIVER CODE
+#initialize instance of puppy
+spot = Puppy.new
+#check that spot can fetch
+spot.fetch("ball")
+spot.speak(4)
+spot.roll_over
+spot.dog_years(5)
+spot.shake("Alison")
