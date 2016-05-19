01713905659dbf8a452f80402f763fecb8045037
hypatiah <danielle.hassid@gmail.com>
2016-05-18 17:01:58 -0700

    Create new kitten class and cat directory

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -24,6 +24,10 @@ class Puppy
   	puts "Nice to meet you #{name}!"
   end
 
+  def initialize
+  	puts "Initializing new puppy instance ..."
+  end
+
 end
 
 
@@ -36,3 +40,40 @@ spot.speak(4)
 spot.roll_over
 spot.dog_years(5)
 spot.shake("Alison")
+
+class Kitten
+	def initialize
+		puts "Initializing new kitten instance ..."
+	end
+
+	def toilet_paper_rolls(integer)
+		puts "Kitten! You unrolled #{integer} rolls of toilet paper!"
+	end
+
+	def cat_lives(integer, misbehavior)
+		puts "Bad kitten! Why did you #{misbehavior}?"
+		lives_left = 9 - integer
+		puts "Now you only have #{lives_left} lives left!"
+	end
+end
+
+#initialize empty array
+cat_directory = []
+#initialize index
+i = 0
+until i > 49
+	kitten = Kitten.new
+	cat_directory << kitten
+	i += 1
+end
+cat_directory.each do |kitten_name|
+	kitten_name.toilet_paper_rolls(6)
+	kitten_name.cat_lives(3, "chew my finger")
+end
+
+
+#DRIVER CODE
+#initialize instance of kitten
+kitten = Kitten.new
+kitten.toilet_paper_rolls(5)
+kitten.cat_lives(3, "pee on my bed")
