4931473c271fd1eede56aba2a1ae45376ef3ec60
hypatiah <danielle.hassid@gmail.com>
2016-05-31 08:12:51 -0700

    Create longphrase function and corrsponding test

--- js/algos.js
+++ js/algos.js
@@ -2,25 +2,32 @@
 // input: array of words or phrases
 // output: returns longest word or phrase in the array
 // steps:
-// initialize counter = 0 and empty hash = {}
+// initialize sample string length = 0
 // iterate through each index of array
 // for each index, set counter = array[index].length
-// 	store array[index] and counter as a key value pair in hash
-// print key associated with highest value
+// if the counter is greater than sample string, return the corresponding array index
+// assign sample string the value of the counter to be used for comparison in next iteration 
+// return array index with the highest counter value
 
 function longphrase(array) {
-	counter = 0;
-	var length_directory = {};
+	//counter = 0;
+	sample_length = 0
+	//var length_directory = {};
+	
 	for (var i=0; i < array.length; i++){
+		// let counter be the length of the element of the array
 		counter = array[i].length;
-		length_directory[array[i]] = counter;
-	}
-	for (var i in length_directory){
-		if(length_directory[i] == max){
-			return i;
+		// if the counter is greater than the sample_length, 
+		if (counter >= sample_length) {
+			result = array[i];
 		}
+		sample_length = counter;	
 	}
+	return result
 }
 
+//Driver code
 array_test = ["long phrase", "longest phrase", "longer phrase"];
 console.log(longphrase(array_test));
+
+
