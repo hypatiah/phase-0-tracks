891f1d94e921fdbcf06818e3ea07eaf2ee52dde0
hypatiah <danielle.hassid@gmail.com>
2016-05-31 09:05:55 -0700

    Create key-value match function and test code

--- js/algos.js
+++ js/algos.js
@@ -1,7 +1,7 @@
-// PSEUDOCODE
-// input: array of words or phrases
-// output: returns longest word or phrase in the array
-// steps:
+// PSEUDOCODE: Find longest phrase
+// Input: array of words or phrases
+// Output: returns longest word or phrase in the array
+// Steps:
 // initialize sample string length = 0
 // iterate through each index of array
 // for each index, set counter = array[index].length
@@ -21,13 +21,41 @@ function longphrase(array) {
 		if (counter >= sample_length) {
 			result = array[i];
 		}
+		//assigns new sample legnth value to = counter so can be used to compare in next iteration
 		sample_length = counter;	
 	}
 	return result
 }
 
-//Driver code
+
+//PSEUDOCODE: Find key-value match
+// Input: 2 objects
+// Output: true if objects share at least 1 key-value pair, false if objects dont
+// Steps:
+// loop through each key of one hash and compare it to all other keys,
+// if the keys are equal, then see if the values are equal, in which case return true, if not return false
+// if the keyse aren't equal to begin with, return false as well
+
+function match(hsh1, hsh2){
+	for (var i=0, j=0; i < hsh1.length; i++, j++) {
+		if (hsh1[i] == hsh2[j]) {
+			if (match.hsh1[i] == match.hsh2[j]) {
+			return true;}
+			else {return false;}
+		}
+		else {return false}
+	}
+}
+
+
+//DRIVER CODE
+//test for longphrase function
 array_test = ["long phrase", "longest phrase", "longer phrase"];
+array2 = ["panda", "cat", "fish", "squirrel"];
 console.log(longphrase(array_test));
+console.log(longphrase(array2));
 
-
+//test for key-value match function
+hash1 = {name: "Tamir", age: 54};
+hash2 = {name: "Steven", age: 54};
+console.log(match(hash1,hash2))
