712ca19559f01d978c2f5ee8d05539acaf1e2d33
hypatiah <danielle.hassid@gmail.com>
2016-05-31 06:14:48 -0700

    Create horse hash compiles colors and names array

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,12 @@
-var colors = ["blue", "green", "rainbow", "sparkles"]
-var names = ["Ed", "Honey", "Sea", "Beatrice"]
+var colors = ["blue", "green", "rainbow", "sparkles"];
+var names = ["Ed", "Honey", "Sea", "Beatrice"];
 
-colors.push("reddyred")
-names.push("Lala")
+colors.push("reddyred");
+names.push("Lala");
+
+var horses = {};
+for (var i = 0; i < colors.length; i++) {
+	horses[names[i]] = colors[i];
+}
+
+console.log(horses);
