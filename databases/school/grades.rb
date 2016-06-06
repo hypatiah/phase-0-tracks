fc8eb9f5c4800765f3c3120879a7d07aca325aa5
hypatiah <danielle.hassid@gmail.com>
2016-06-06 09:51:18 -0700

    Create working class db generator

--- databases/school/grades.rb
+++ databases/school/grades.rb
@@ -6,34 +6,34 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("class.db")
+db.results_as_hash = true
 
-create_course_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS courses(
-    id INTEGER PRIMARY KEY,
-    course_name VARCHAR(255),
-SQL
+# create_course_table_cmd = <<-SQL
+#   CREATE TABLE IF NOT EXISTS courses(
+#     id INTEGER PRIMARY KEY,
+#     course_name VARCHAR(255),
+# SQL
 
 create_class_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS class(
     id INTEGER PRIMARY KEY,
     student VARCHAR(255),
-    course_id VARCHAR(255),
-    grade INT,
-    FOREIGN KEY (course_id) REFERENCES courses(id)
-  )
+    course VARCHAR(255),
+    grade INT
+  );
 SQL
-
-db.execute(create_course_table_cmd)
+# FOREIGN KEY (course_id) REFERENCES courses(id)
+# db.execute(create_course_table_cmd)
 db.execute(create_class_table_cmd)
 
-def create_course(db, name)
-	db.execute("INSERT INTO courses (name) VALUES (?)", [name])
-end
+# def create_course(db, name)
+# 	db.execute("INSERT INTO courses (name) VALUES (?)", [name])
+# end
 
 def create_class(db, student, course, grade)
-  db.execute("INSERT INTO class (student, course, grade) VALUES (?, ?, ?)", [student, course, grade])
+  db.execute("INSERT INTO class (student, course, grade) VALUES (?, ?, ?);", [student, course, grade])
 end
 
 100.times do
-  create_class(db, Faker::Name.name, Faker::Educator.course, Faker::Number.decimal(2))
+  create_class(db, Faker::Name.name, Faker::Commerce.department, Faker::Number.decimal(2))
 end
