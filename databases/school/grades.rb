a9f77516e408622e4994e5e892af24eaabc41d4f
hypatiah <danielle.hassid@gmail.com>
2016-06-07 16:20:28 -0700

    Remove comments unnecessary

--- databases/school/grades.rb
+++ databases/school/grades.rb
@@ -18,21 +18,12 @@ create_test_table_cmd = <<-SQL
   );
 SQL
 
- # FOREIGN KEY (course_id) REFERENCES courses(id) <- goes inside table syntax above if course_table_cmd working
-# db.execute(create_course_table_cmd)
-db.execute(create_class_table_cmd)
+db.execute(create_test_table_cmd)
 
 #method to add a insert a student's test scores
 def create_test(db, student_first, student_last, grade)
   db.execute("INSERT INTO test (student_first, student_last, grade) VALUES (?, ?, ?);", [student_first, student_last, grade])
 end
-
-
-# #add 100 students info to database with randomized data
-# 100.times do
-# 	# create_course(db, Faker::Commerce.department)
-# 	create_class(db, Faker::Name.name, Faker::Commerce.department, Faker::Number.decimal(2))
-# end
 
 #USER INTERFACE
 response = ""
